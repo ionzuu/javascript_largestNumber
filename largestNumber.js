f7250827f2d8e6f17dd839379d59bde533d2e002
Jonathan Costilla <99742932+ionzuu@users.noreply.github.com>
2024-09-29 22:16:20 -0500

    Add files via upload

---
+++ largestNumber.js
@@ -0,0 +1,7 @@
+// Task 6: Find the Largest Number in an Array
+// Write a function that takes an array and returns the largest number.
+
+function largestNumber(arr) {
+    return (console.log("Array: ", arr, "Largest Number: ", Math.max(...arr)))
+}
+largestNumber([1, 2 , 3, 8, 74, 88, 23, 100])
